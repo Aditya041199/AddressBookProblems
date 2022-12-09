81fd46e987283251af4797448baded9efa3fb456
aditya patil <Aditya@example.com>
2022-12-09 09:53:12 +0530

    [Aditya]Add:Edit Contact

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -10,15 +10,18 @@ namespace AddressBook
             book.CreateContact();
             while(true)
             {
-                Console.WriteLine("1. Create Contact\n2. Display Contact\n3. Exit");
+                Console.WriteLine("1. Create Contact\n2. Display Contact\n3. Edit Contact");
                 int choice = Convert.ToInt32(Console.ReadLine());
                 switch(choice)
                 {
                     case 1:
                         book.CreateContact();
                         break;
-                        case 2:
+                    case 2:
                         book.DisplayContact();
+                        break;
+                    case 3:
+                        book.EditContact();
                         break;
                 }
             }
